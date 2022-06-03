533084a8f5c508c1d6f933dca5681a246ff28bb8
Muluken Hakim <mulukenhakim97gmail.com>
2022-06-03 11:07:36 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int ch;
+
+	ch = 122;
+	i = 26;
+	while (i > 0)
+	{										putchar(ch);
+		 ch--;
+		 i--;								}
+	putchar(10);
+	return (0);
+}
