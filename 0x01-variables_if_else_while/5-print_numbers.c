2c4332e8adca74cb0b3146500b76121dea866de4
Muluken Hakim <mulukenhakim97gmail.com>
2022-06-03 10:47:28 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	while (i < 10)
+	{										printf("%d", i);
+		i++;								}
+	putchar(10);
+	return (0);
+}
