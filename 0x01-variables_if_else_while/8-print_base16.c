9a2e881f2464570bd36cd28f415b1fa848b26546
Muluken Hakim <mulukenhakim97gmail.com>
2022-06-03 11:22:01 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char ch;
+	
+	i = 0;
+	ch = 97;
+	while (i < 16)
+	{										if (i > 9)
+		 {										putchar(ch)
+			 ch++;
+			 i++;								}								       else
+		 {										putchar(i + '0');
+			 i++;								}								}
+	  putchar(10);
+	  return (0);
+}
