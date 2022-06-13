8c9e480119b7228e22277c1aaf70dff5b77720b2
Muluken Hakim <mulukenhakim97gmail.com>
2022-06-13 09:40:59 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string, followed by a new line, to stdout.
+ * _puts - Prints a string, followed by a new line, to stdout.
  *
  * @str: This is my input string
  *
