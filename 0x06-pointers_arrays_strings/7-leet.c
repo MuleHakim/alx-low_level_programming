d587c67d849e59656c691875d085f6a2acde9b7b
Muluken Hakim <mulukenhakim97gmail.com>
2022-06-16 11:07:52 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,15 +1,11 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337.
- * letters a and A are replaced by 4.
- * Letters e and E are replaced by 3
- * Letters o and O are replaced by 0
- * Letters t and T are replaced by 7
- * Letters l and L are replaced by 1
- * @s: pointer to string.
- * 
- * Return: pointer to s.
+ * leet - Write a function that encodes a string into 1337
+ *
+ * @changed: This is the input string
+ *
+ * Return: String converted to 1337
  */
 
 char *leet(char *s)
