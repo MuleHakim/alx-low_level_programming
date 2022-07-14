83242a03532a47db97d3fbb6d5d806f764f4caa5
MuleHakim <mulukenhakim97@gmail.com>
2022-07-14 07:17:12 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,21 +18,25 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	temp = malloc(sizeof(list_t));
+
 	if (temp == NULL)
 	{
 		return (NULL);
 	}
 
 	temp->str = strdup(str);
+
 	if (temp->str == NULL)
 	{
 		free(temp);
 		return (NULL);
 	}
+
 	while (str[length])
 	{
 		length++;
 	}
+
 	temp->len = length;
 	temp->next = NULL;
 
@@ -43,10 +47,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	temp2 = *head;
+
 	while (temp2->next)
 	{
 		temp2 = temp2->next;
 	}
+
 	temp2->next = temp;
+
 	return (temp);
 }
