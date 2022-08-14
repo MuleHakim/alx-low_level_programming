44f2ede4588f6824a73bf1244626c26d543dc188
MuleHakim <mulukenhakim97@gmail.com>
2022-08-14 01:05:10 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with constant byte.
- * @s: array of char
- * @b: array of chat
- * @n: number of bytes
- * Return:(s) 
+ * _memset - prints buffer in hexa
+ * @s: buffer
+ * @b: write
+ * @n: size
+ * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
